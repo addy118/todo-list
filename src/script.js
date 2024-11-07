b03b951f1cd7e1d80a7de5bfa0e5964034f6f878
Aditya Kirti <addyyy118@gmail.com>
2024-11-07 20:33:38 +0530

    Make initial modification

--- src/script.js
+++ src/script.js
@@ -1,12 +1,1 @@
 import './style.css'
-import js from './assets/js.png'
-
-console.log('working!')
-
-const image = document.createElement('img');
-image.src = js;
-image.alt = 'js-logo';
-image.width = 100;
-image.height = 100;
-
-document.querySelector('.logos').appendChild(image);
