b2629b9e5ba0de3d95d6c96c9d5eeac8c8f798c6
Aditya Kirti <addyyy118@gmail.com>
2024-11-14 16:43:19 +0530

    Add webpack back

--- src/script.js
+++ src/script.js
@@ -1,3 +1,8 @@
+import './styles/style.css';
+import './styles/projects.css';
+import './styles/todos.css';
+import './styles/modal.css';
+
 import { UserUI } from './classes/ui/UserUI.js';
 import { User } from './classes/User.js';
 import { Local } from './classes/LocalStorage.js';
